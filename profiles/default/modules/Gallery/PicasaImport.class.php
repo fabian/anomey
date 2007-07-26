5258b4d732de5cc1b4f2b1985fff02673e00ba54
Fabian Vogler <fabian@equivalence.ch>
2007-07-26 10:02:58 +0000

    Updated copyright notice to GPL v3.

--- profiles/default/modules/Gallery/PicasaImport.class.php
+++ profiles/default/modules/Gallery/PicasaImport.class.php
@@ -1,19 +1,18 @@
 <?php
 
-
 /*
  * anomey 2.1 - content management
  * ================================
  * 
- * Copyright (C) 2006 - Adrian Egloff (adrian@anomey.ch), 
- * Cyril Gabathuler (cyril@anomey.ch) and Fabian Vogler (fabian@anomey.ch)
+ * Copyright © 2006, 2007 - Adrian Egloff <adrian@anomey.ch>, 
+ * Cyril Gabathuler <cyril@anomey.ch> and Fabian Vogler <fabian@anomey.ch>
  * 
  * This file is part of anomey. For more information about anomey
- * visit http://anomey.ch/ or write a mail to info@anomey.ch.
+ * visit <http://anomey.ch/> or write a mail to <info@anomey.ch>.
  * 
  * anomey is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  * 
  * anomey is distributed in the hope that it will be useful,
@@ -22,9 +21,7 @@
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with anomey (license.txt); if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
- * or visit http://www.gnu.org/copyleft/gpl.html.
+ * along with anomey (license.txt). If not, see <http://www.gnu.org/licenses/>.
  */
  
 /**
